782ca6b0e7ed68ce2757e94c0ad8d4ab3cc8fa4c
MalcolmDahling <mac.dahling@gmail.com>
2021-12-15 17:00:39 +0100

    Done

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let end = new Date('2022-09-23T17:00:00');
 
 setInterval(function(){
     let today = new Date();
+    let diff = end - today;
 
-    let days    = Math.floor( end.valueOf()/1000/60/60/24 - today.valueOf()/1000/60/60/24 );
-    let hours   = Math.floor( end.valueOf()/1000/60/60    - today.valueOf()/1000/60/60    ) - days*24;
-    let minutes = Math.floor( end.valueOf()/1000/60       - today.valueOf()/1000/60       ) - days*24*60; //fel
-    let seconds = Math.floor( end.valueOf()/1000          - today.valueOf()/1000          ) - days*24*60*60; //fel
+    let days = Math.floor(diff/1000/60/60/24);
+    let hours = Math.floor(diff/1000/60/60 - days*24);
+    let minutes = Math.floor(diff/1000/60 - days*24*60 - hours*60);
+    let seconds = Math.floor(diff/1000 - days*24*60*60 - hours*60*60 - minutes*60);
+
+
     
     document.getElementById('daySpan').innerHTML    = days;
     document.getElementById('hourSpan').innerHTML   = hours;
